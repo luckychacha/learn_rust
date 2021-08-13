8290088def5f99c8b8de34199a20e9d37325fece
leixinxin <luckychacha@163.com>
2021-08-13 18:11:50 +0800

    format

--- code_wars/src/five_kyu/primes.rs
+++ code_wars/src/five_kyu/primes.rs
@@ -36,7 +36,6 @@ pub fn puzzle(s: i32) -> i32 {
     count
 }
 
-
 fn find_k(n: i32) -> i32 {
     let mut n = n;
     let mut res: i32 = 0;
@@ -57,3 +56,35 @@ fn find_k(n: i32) -> i32 {
     }
     res
 }
+
+/*
+fn prime_factor_count(n: i32) -> i32 {
+    let (mut n, mut candidate, mut count) = (n, 2, 0);
+    while n > 1 {
+        while n % candidate == 0 {
+            count += 1;
+            n /= candidate;
+        }
+        candidate += 1;
+    }
+    count
+}
+
+fn count_kprimes(k: i32, start: i32, end: i32) -> Vec<i32> {
+    (start..=end).filter(|&n| prime_factor_count(n) == k).collect()
+}
+
+fn puzzle(s: i32) -> i32 {
+    let v_1 = count_kprimes(1, 2, s);
+    let v_3 = count_kprimes(3, 8, s);
+    let v_7 = count_kprimes(7, 128, s);
+
+    v_7.into_iter()
+        .map(|c| {
+            v_3.iter()
+                .filter(|&b| v_1.iter().any(|&a| a + b + c == s))
+                .count()
+        })
+        .sum::<usize>() as i32
+}
+*/
