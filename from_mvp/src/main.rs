c1392a080282def9797b2f06d05e428af9bb99a5
luckychacha <358140284@qq.com>
2021-10-23 17:07:47 +0800

    init pattern match

--- from_mvp/src/main.rs
+++ from_mvp/src/main.rs
@@ -1,10 +1,11 @@
-use object_oriented::components::{Draw, Screen};
+use object_oriented::{components::{Draw, Screen}};
 
-use crate::object_oriented::components::Button;
+use crate::object_oriented::{components::Button, enhanced_post::Post};
 
 mod smart_pointers;
 pub mod no_fear_concurrent_and_parallel;
 pub mod object_oriented;
+pub mod pattern_match;
 
 fn main() {
     // smart_pointers::box_concept::box_concept();
@@ -19,25 +20,37 @@ fn main() {
     // no_fear_concurrent_and_parallel::cross_thread_send_message::channel_demo();
     // no_fear_concurrent_and_parallel::share_status_concurrency::mutex_demo();
 
-    let screen = Screen {
-        components: vec![
-            Box::new(Button {
-                width: 10,
-                height: 5,
-                label: String::from("button-label"),
-            }),
-            Box::new(SelectBox {
-                width: 20,
-                height: 5,
-                options: vec![
-                    String::from("left"),
-                    String::from("mid"),
-                    String::from("right"),
-                ],
-            })
-        ],
-    };
-    screen.run();
+    // let screen = Screen {
+    //     components: vec![
+    //         Box::new(Button {
+    //             width: 10,
+    //             height: 5,
+    //             label: String::from("button-label"),
+    //         }),
+    //         Box::new(SelectBox {
+    //             width: 20,
+    //             height: 5,
+    //             options: vec![
+    //                 String::from("left"),
+    //                 String::from("mid"),
+    //                 String::from("right"),
+    //             ],
+    //         })
+    //     ],
+    // };
+    // screen.run();
+    // 状态模式的 Post
+    // let mut post = Post::new();
+    // post.add_text("test");
+    // post.request_review();
+    // post.approve();
+    // println!("{}", post.content());
+    // 优化后的 Post
+    let mut enhanced_post_draft = Post::new();
+    enhanced_post_draft.add_text("enhanced post content");
+    let enhanced_post_pending = enhanced_post_draft.request_review();
+    let enhanced_post = enhanced_post_pending.approve();
+    println!("{}", enhanced_post.content());
 }
 
 pub struct SelectBox {
