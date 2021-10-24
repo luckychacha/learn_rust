b1e4f02e04c570470cad116169c93bb34891d60b
luckychacha <358140284@qq.com>
2021-10-24 13:59:57 +0800

    模式匹配

--- from_mvp/src/pattern_match/pattern_match.rs
+++ from_mvp/src/pattern_match/pattern_match.rs
@@ -22,6 +22,26 @@
 //          - else if
 //          - else if let
 //      - 但，if let 不会检查穷尽性
+// while let 条件循环
+//  - 只要模式继续满足匹配的条件，那它允许 while 循环一直运行
+// for 循环
+//  - for 循环中，模式就是紧随 for 关键字后的值
+// let 也是一种模式
+// 函数的参数也是模式
+// 模式的两种形式
+//  - 可辩驳的【refutable pattern】、无可辩驳的【irrefu pattern 】
+//  - 能匹配任何可能传递的值的模式：无可辩驳的【let x = 5;】
+//  - 对于某些可能的值，无法进行匹配的模式：可辩驳的【if let Some(x) = a_value】，如果右边是 None 就会失败
+//  - 函数参数、let、for 只接受无可辩驳的模式
+//  - if let 和 while let 接收可辩驳和无可辩驳的模式
+// 匹配字面值：
+// let x = 1;
+// match x {
+//      1 => println!("1"),
+//      2 => println!("2"),
+//      _ => println!("others"),
+// }
+// 匹配命名变量
 pub fn patter_demo() {
     let color: Option<&str> = None;
     let is_tuesday = false;
@@ -39,5 +59,18 @@ pub fn patter_demo() {
         }
     } else {
         println!("Using blus as the background color");
+    }
+
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a' ..='j' => println!("early ASCII letter"),
+        'k' ..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
     }
 }
